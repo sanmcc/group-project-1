f229bb1dc72d8b65c07da7552124565562cd49f6
Danny <dahneel@gmail.com>
2023-02-09 21:36:42 -0700

    got functions to pull cat pic and dog pic and added button events

--- Assets/develop/script.js
+++ Assets/develop/script.js
@@ -1,37 +1,22 @@
+fightButton = document.getElementById("fightButton")
 
+function onClick($this) {
+  var catBreed = $this.value;
+  if (catBreed == ''){
+     console.log("no input");
+  } else {
+      localStorage.setItem("kitty", catBreed);
+}};
 
-function getKitty(catBreed){
-  var queryCatURL = "https://api.thecatapi.com/v1/images/search?breed_ids=" + catBreed 
-  const kitty = document.getElementById("kitty")
-  fetch(queryCatURL)
-     .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-      JSON.stringify(data)   
-      var kittyURL = data[0].url
-      var image = document.createElement("img")
-      image.setAttribute("src", kittyURL);
-      kitty.appendChild(image);
-    })
-  }
-function getDoggo(dogBreed) {
-  const doggo = document.getElementById("doggo")
-  var queryDogURL = "https://dog.ceo/api/breed/" + dogBreed + "/images"
+function onClick2($this) {
+  var dogBreed = $this.value;
+  if (dogBreed == ''){
+     console.log("no input");
+  } else {
+      localStorage.setItem("doggo", dogBreed);
+}};
 
-  fetch(queryDogURL)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-    JSON.stringify(data)
-    console.log(data)
-    var doggoURL = data.message[3]
-    console.log(doggoURL)
-    var image = document.createElement("img")
-    image.setAttribute("src", doggoURL);
-    doggo.appendChild(image);
-})}
-getKitty(catBreed);
-getDoggo(dogBreed);
- 
+fightButton.addEventListener("click", function() {
+  var fightPage = "./search-results.html"
+  location.assign(fightPage)
+});
---
+++ Assets/develop/script2.js
@@ -0,0 +1,37 @@
+function getKitty(catBreed){
+    var queryCatURL = "https://api.thecatapi.com/v1/images/search?breed_ids=" + catBreed 
+    const kitty = document.getElementById("kitty")
+    fetch(queryCatURL)
+       .then(function(response) {
+          return response.json();
+      })
+      .then(function(data) {
+        JSON.stringify(data)   
+        var kittyURL = data[0].url
+        var image = document.createElement("img")
+        image.setAttribute("src", kittyURL);
+        kitty.appendChild(image);
+      })
+    }
+
+function getDoggo(dogBreed) {
+    const doggo = document.getElementById("doggo")
+    var queryDogURL = "https://dog.ceo/api/breed/" + dogBreed + "/images"
+  
+    fetch(queryDogURL)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+      JSON.stringify(data)
+      console.log(data)
+      var doggoURL = data.message[3]
+      console.log(doggoURL)
+      var image = document.createElement("img")
+      image.setAttribute("src", doggoURL);
+      doggo.appendChild(image);
+  })}
+
+getKitty(localStorage.getItem("kitty"));
+getDoggo(localStorage.getItem("doggo"));
+
