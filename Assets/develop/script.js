97bf2a25c873397b2654b660b6fb17e2da8e9944
itlleat <scott.schulman84@gmail.com>
2023-02-12 18:57:38 -0700

    borders

--- Assets/develop/script.js
+++ Assets/develop/script.js
@@ -1,60 +1,54 @@
-fightButton = document.getElementById("fightButton")
+fightButton = document.getElementById("fightButton");
 
 function onClick($this) {
   var catBreed = $this.value;
-  if (catBreed == ''){
-     console.log("no input");
-    
+  if (catBreed == "") {
+    console.log("no input");
   } else {
-  
-      localStorage.setItem("kitty", catBreed);
-      
-}};
+    // document.getElementById("dogB").disabled = true;
+    localStorage.setItem("kitty", catBreed);
+  }
+}
 
 function onClick2($this) {
   var dogBreed = $this.value;
-  if (dogBreed == ''){
-     console.log("no input");
-
+  if (dogBreed == "") {
+    console.log("no input");
   } else {
-      localStorage.setItem("doggo", dogBreed);
-     
-}};
+    localStorage.setItem("doggo", dogBreed);
+  }
+}
 
-fightButton.addEventListener("click", function() {
-  var fightPage = "./search-results.html"
-  location.assign(fightPage)
-  
+fightButton.addEventListener("click", function () {
+  var fightPage = "./search-results.html";
+  location.assign(fightPage);
 });
 
-
-
 var imgProp = {
- 'padding': '3px',
- 'backgroundColor': '#eded01',
- 'borderSize': '1ps',
- 'borderStyle': 'inset',
- 'borderColor': 'red'
+  padding: "3px",
+  backgroundColor: "#eded01",
+  borderSize: "1ps",
+  borderStyle: "inset",
+  borderColor: "red",
 };
 
 function highlightImg() {
+  // document.getElementById("dogB").disabled = true;
 
- var allimgs = document.getElementsByTagName('img');
- var nrallimgs = allimgs.length;
+  var allimgs = document.getElementsByTagName("img");
+  var nrallimgs = allimgs.length;
 
+  for (i = 0; i < nrallimgs; i++) {
+    allimgs[i].onclick = function () {
+      this.style.padding = imgProp.padding;
+      this.style.backgroundColor = imgProp.backgroundColor;
+      this.style.borderSize = imgProp.borderSize;
+      this.style.borderStyle = imgProp.borderStyle;
+      this.style.borderColor = imgProp.borderColor;
+    };
+  }
 
- for(i=0; i<nrallimgs; i++) {
- allimgs[i].onclick=function() {
-
- this.style.padding = imgProp.padding;
- this.style.backgroundColor = imgProp.backgroundColor;
- this.style.borderSize = imgProp.borderSize;
- this.style.borderStyle = imgProp.borderStyle;
- this.style.borderColor = imgProp.borderColor;
- }
- }
-
-}
+  }
 
 
 highlightImg();
